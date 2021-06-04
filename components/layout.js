12bc973ce99b16e43b868e7efc69853bcce09619
Patrick Roth <patrockdev@icloud.com>
2021-06-04 11:47:54 +0200

    border image size change

--- components/layout.js
+++ components/layout.js
@@ -32,8 +32,8 @@ export default function Layout({ children, home }) {
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={400}
+              width={400}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -46,8 +46,8 @@ export default function Layout({ children, home }) {
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
+                  height={208}
+                  width={208}
                   alt={name}
                 />
               </a>
